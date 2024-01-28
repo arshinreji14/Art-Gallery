33017445f2208841dcbd62feeafb9cc6633a2ea5
arshinreji <arshinreji9947@gmail.com>
2024-01-28 20:19:24 +0530

    watch page bug fixed

--- art-gallery/app/page.tsx
+++ art-gallery/app/page.tsx
@@ -1,10 +1,12 @@
 import Hero from "@/components/Hero/Hero";
+import Showcaselist from "@/components/Showcase/Showcaselist";
 
 
 export default function Home() {
   return (
     <main className="">   
     <Hero/>
+    <Showcaselist/>
     </main>
   )
 }
